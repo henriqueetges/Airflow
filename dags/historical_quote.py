596276320c47366d04f2aee11b12d32f4a20f88c
Henrique Etges <ike.etges@gmail.com>
2025-02-06 15:18:56 -0300

    added the task to push last records to production incrementally

--- dags/historical_quote.py
+++ dags/historical_quote.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 @dag(schedule="@weekly", start_date=datetime(2024, 1, 1), catchup=False)
 def fetch_multiple_tickers():
+
     http_sensor_task = HttpSensor(
         task_id = "check_api",
         http_conn_id="brapi",
@@ -19,10 +20,21 @@ def fetch_multiple_tickers():
 
     db_sensor_task = PostgresOperator(
         task_id="test_postgres_connection",
-        postgres_conn_id="local_pg",  # Change this if using a different connection
+        postgres_conn_id="local_pg", 
         sql="SELECT 1;")
     
+    truncate_stg = PostgresOperator(
+        task_id="truncate_history_stg",
+        postgres_conn_id="local_pg_stg", 
+        sql="TRUNCATE TABLE inv_stg.public.stg_stock_quotes_history RESTART IDENTITY CASCADE;"
+    )
     
+    delete_week_data = PostgresOperator(
+        task_id="delete7days",
+        postgres_conn_id="local_pg", 
+        sql="DELETE FROM inv.public.stock_quotes_history WHERE date >= now() - INTERVAL '7 DAYS'"
+    )
+
     @task
     def fetch_list_of_tickers():
         hook = PostgresHook(postgres_conn_id='local_pg')
@@ -48,26 +60,41 @@ def fetch_multiple_tickers():
     def aggregate_results(results):
         dfs = [pd.DataFrame(result) for result in results]
         final_df = pd.concat(dfs, ignore_index=True)
-        final_df['date'] = pd.to_datetime(final_df['date'])
+        final_df['date'] = pd.to_datetime(final_df['date'], unit='s')
         final_df['loaded_at'] = datetime.now()
         return pd.DataFrame(final_df)
     
     @task
     def push_to_stg(df):
         hook = PostgresHook(postgres_conn_id='local_pg_stg')
-        table = ' inv_stg.public.stg_stock_quotes_history'
+        table = 'inv_stg.public.stg_stock_quotes_history'
         columns = [c for c in df.columns]
         data_tuple = [tuple(x) for x in df[columns].values]
-        hook.insert_rows(table=table, rows=data_tuple, target_fields=columns)
+        hook.insert_rows(table=table, rows=data_tuple, target_fields=columns) 
+
+    @task
+    def insert_into_prod():
+        prod = PostgresHook(postgres_conn_id='local_pg')
+        stg = PostgresHook(postgres_conn_id='local_pg_stg')
+        query = f"SELECT * FROM inv_stg.public.stg_stock_quotes_history WHERE date >= now() - INTERVAL '7 DAYS'"
+        results = stg.get_pandas_df(query)
+        results['date'] = results['date'].astype(str)
+        results['loaded_at'] = results['loaded_at'].astype(str)
+        columns = [c for c in results.columns]
+        tuples = [tuple(x) for x in results[columns].values]
+        prod.insert_rows(table='inv.public.stock_quotes_history', rows=tuples, target_fields=columns)
+        return tuples
+        
     
     tickers = fetch_list_of_tickers()
     fetch_tasks = fetch_data_from_api.expand(ticker=tickers)   
     aggregated = aggregate_results(fetch_tasks)
-    insert_task = push_to_stg(aggregated)
+    insert_stg = push_to_stg(aggregated)
+    insert_prod = insert_into_prod()
 
-    http_sensor_task >> fetch_tasks
-    db_sensor_task >> tickers >> fetch_tasks 
-    fetch_tasks >>  aggregated >> insert_task
+    http_sensor_task >> tickers >> fetch_tasks >> aggregated >> truncate_stg >> insert_stg >> delete_week_data >> insert_prod
+    db_sensor_task >> tickers  >> fetch_tasks >> aggregated >> truncate_stg >> insert_stg >> delete_week_data >> insert_prod
+    
 
 fetch_multiple_tickers()
     
