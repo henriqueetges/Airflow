2178b57d1891836f795a339ec1dc84b502dd6a89
Henrique Etges <ike.etges@gmail.com>
2025-02-06 11:54:43 -0300

    added the ability to push the data into stage

--- dags/historical_quote.py
+++ dags/historical_quote.py
@@ -1,14 +1,9 @@
-import json
-from airflow import DAG
 from airflow.decorators import task, dag
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.operators.python import PythonOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.hooks.http import HttpHook
 from datetime import datetime
 import pandas as pd
-
-TICKERS = ['BBAS3', 'PETR4']
 
 @dag(schedule=None, start_date=datetime(2024, 1, 1), catchup=False)
 def fetch_multiple_tickers():
@@ -20,6 +15,14 @@ def fetch_multiple_tickers():
         timeout=20,
         mode="poke"
     )
+    
+    @task
+    def fetch_list_of_tickers():
+        hook = PostgresHook(postgres_conn_id='local_pg')
+        sql = 'SELECT DISTINCT ticker from inv.public.transac'
+        results = hook.get_records(sql)
+        tickers = [row[0] for row in results]
+        return tickers
 
     @task
     def fetch_data_from_api(ticker):
@@ -30,48 +33,36 @@ def fetch_multiple_tickers():
             "interval": "1d",
         }
         response = http_hook.run(endpoint=endpoint, data=params).json()
-        return response
+        historical_records = response.get('results')[0].get('historicalDataPrice')
+        historical_records = [{**d, 'ticker': ticker} for d in historical_records]
+        return historical_records
     
     @task
     def aggregate_results(results):
         dfs = [pd.DataFrame(result) for result in results]
         final_df = pd.concat(dfs, ignore_index=True)
-        return final_df.to_json()
+        final_df['date'] = pd.to_datetime(final_df['date'])
+        final_df['loaded_at'] = datetime.now()
+        return pd.DataFrame(final_df)
     
-    fetch_tasks = []
-    for ticker in TICKERS:
-        fetch_task = fetch_data_from_api.override(task_id=f"fetch {ticker} from BRAPI")(ticker)
-        fetch_tasks.append(fetch_task)
+    @task
+    def push_to_stg(df):
+        hook = PostgresHook(postgres_conn_id='local_pg_stg')
+        table = ' inv_stg.public.stock_quotes_history'
+        columns = [c for c in df.columns]
+        data_tuple = [tuple(x) for x in df[columns].values]
+        query = f"""
+                INSERT INTO {table} ({[x for x in columns]})
+                VALUES %s
+                """
+        hook.insert_rows(table=table, rows=data_tuple, target_fields=columns)
+    
+    tickers = fetch_list_of_tickers()
+    fetch_tasks = fetch_data_from_api.expand(ticker=tickers)   
     aggregated = aggregate_results(fetch_tasks)
+    insert_task = push_to_stg(aggregated)
 
-    http_sensor_task >> fetch_tasks >> aggregated
+    http_sensor_task >> tickers >> fetch_tasks >> aggregated >> insert_task
 
 fetch_multiple_tickers()
     
-    # @task
-    # def save_to_db(ti):
-    #     pass
-    #     # data = ti.xcom_ull(task_ids="fetch_api_data")
-    #     # if not data:
-    #     #     print('No new data')
-    #     #     return
-    #     # hook = PostgresHook(postgres_conn_id = 'local_pg')
-    #     # with hook.get_conn() as conn:
-    #     #     with conn.cursor() as cur:
-    #     #         for record in data:
-    #     #             cur.execute('INSERT INTO inv_stg.public.stock_quotes_history (ticker, date, open, low, high, volume, adjusted_close, loaded_at) \
-    #     #                          VALUES (%s,  %s, %s, %s, %s, %s, %s, %s)'), (record[])
-    #     #     conn.commit()
-    #     #     print('Data added')
-
-    # @task
-    # def get_max_date():
-    #     hook = PostgresHook(postgres_conn_id='local_pg')
-    #     with hook.get_conn() as conn:
-    #         with conn.cursor() as cur:
-    #             cur.execute('SELECT MAX(date) from inv.public.stock_quotes_history')
-    #             max_date = cur.fetchone()[0]
-    #             print(f"Last record is as if {max_date}")
-    #     return max_date or datetime.now()
-
-
