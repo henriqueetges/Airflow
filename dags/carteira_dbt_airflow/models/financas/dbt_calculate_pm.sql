994c72af5c5f927ede12987d16feb1ffc860cf24
Henrique Etges <ike.etges@gmail.com>
2025-02-10 17:01:16 -0300

    made changes to the PM and portfolio model so that they captured values for each day

--- dags/carteira_dbt_airflow/models/financas/dbt_calculate_pm.sql
+++ dags/carteira_dbt_airflow/models/financas/dbt_calculate_pm.sql
@@ -1,17 +1,37 @@
+{% set dates = get_min_max_date('inv.public.stock_quotes_history', 'date') %}
+
+
 with totals as (
 SELECT 
+transac_date,
 ticker, 
 sum(quantity * price)::numeric AS total_position,
 sum(quantity)::integer AS total_quantity
 FROM inv.public.transac
-GROUP BY ticker
-having sum(quantity) > 0
-)
+GROUP BY ticker, transac_date
+),
+
+interval as (
+    /* CALCULATES A PERIOD BETWEEN MY FIRST TRANSACTION THE LAST PRICE I HAVE CAPTURED */
+    SELECT
+        date_trunc('day', dd)::date AS dt
+    FROM generate_series(
+        (SELECT MIN(transac_date) from inv.public.transac)::timestamp
+        , '{{ dates["max_date"] }}'::timestamp
+        , '1 day'::interval) dd)
 
 
-SELECT 
+
+
+
+SELECT
+I.dt, 
 ticker, 
-ROUND(total_position / total_quantity, 2) as PM,
-total_quantity::integer
+sum(total_position)::numeric  AS total_position,
+sum(total_quantity)::numeric  AS total_quantity,
+(sum(total_position)  / nullif(sum(total_quantity), 0))::numeric  AS pm
 
-FROM totals
+FROM interval I
+LEFT JOIN totals T
+    ON I.dt >= T.transac_date
+group by 1, 2
--- dags/carteira_dbt_airflow/models/financas/dbt_porfolio_returns.sql
+++ dags/carteira_dbt_airflow/models/financas/dbt_porfolio_returns.sql
@@ -1,6 +1,12 @@
-{% set dates = get_min_max_date('inv.public.stock_quotes_history', 'date') %}
-/* I WANT TO USE THIS MODEL TO GENERATE A RANGE OF DAYS AND BE ABLE TO CALCULATE RETURNS FOR EACH STOCK ON THAT DAY, BASED ON IT'S CLOSING PRICES */
-
 SELECT 
-    '{{ dates["min_date"] }}' as min_date
-    , '{{ dates["max_date"] }}' as max_date 
+    pm.*
+    , (sh.open * pm.total_quantity)::numeric AS position_open
+    , (sh.close * pm.total_quantity)::numeric AS position_close
+    , (sh.high* pm.total_quantity)::numeric AS position_high
+	, pm.total_position - (sh.open * pm.total_quantity)::numeric AS pl_open
+	, pm.total_position - (sh.close * pm.total_quantity)::numeric AS pl_close
+	, pm.total_position - (sh.high * pm.total_quantity)::numeric AS pl_high
+FROM dbt.dbt_calculate_pm pm
+INNER JOIN inv.public.stock_quotes_history SH
+        ON SH.date::date = pm.dt::date
+        AND SH.ticker = PM.ticker
