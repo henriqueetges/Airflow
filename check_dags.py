00549ca58e600538a7107f5c367d0bead515edfb
Henrique Etges <ike.etges@gmail.com>
2025-03-06 17:54:48 -0300

    another worflow for dags 1

---
+++ check_dags.py
@@ -0,0 +1,11 @@
+import os
+from airflow.models import DagBag
+
+def check_dags()
+    dag_bag = DagBag()
+    if dag_bag.import_errors:
+        raise Exception(f'Import errors occurred, {dag_bag.import_errors}')
+    print('All DAGs Imported')
+
+if __name__ == '__main__':
+    check_dags()
