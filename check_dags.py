fed3ed3134c646906040ede593fc4c8e998d6748
Henrique Etges <ike.etges@gmail.com>
2025-03-06 23:01:34 -0300

    another wofklow for dags 6

--- check_dags.py
+++ check_dags.py
@@ -1,8 +1,10 @@
 import os
 from airflow.models import DagBag
 
+DAG_FOLDER = os.path.join(os.getenv("GITHUB_WORKSPACE", ""), "dags")
+
 def check_dags():
-    dag_bag = DagBag()
+    dag_bag = DagBag(dag_folder=DAG_FOLDER)
     if dag_bag.import_errors:
         raise Exception(f'Import errors occurred, {dag_bag.import_errors}')
     print('All DAGs Imported')
