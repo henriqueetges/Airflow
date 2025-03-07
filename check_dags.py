dce8235718470e755494fb5e041df13ebfb30bcc
Henrique Etges <ike.etges@gmail.com>
2025-03-06 23:12:28 -0300

    tring to avoid connection issues 2

--- check_dags.py
+++ check_dags.py
@@ -5,7 +5,7 @@ DAG_FOLDER = os.path.join(os.getenv("GITHUB_WORKSPACE", ""), "dags")
 
 def check_dags():
     dag_bag = DagBag(dag_folder=DAG_FOLDER, include_examples=False)
-    ignored_errors = ("airflow.exceptions.AirflowNotFoundException", "sqlalchemy.exc.OperationalError")
+    ignored_errors = ["airflow.exceptions.AirflowNotFoundException", "sqlalchemy.exc.OperationalError"]
 
     import_errors = {
         dag_id: error for dag_id, error in dag_bag.import_errors.items()
