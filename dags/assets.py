e0842f28ed1ad75179a43a0515480f2471716a49
Henrique Etges <ike.etges@gmail.com>
2025-04-01 14:59:41 -0300

    feat: adding the ability to pull all available assets from api

---
+++ dags/assets.py
@@ -0,0 +1,72 @@
+from airflow.decorators import task, dag
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.hooks.http import HttpHook
+from datetime import datetime
+import pandas as pd
+
+
+@dag(
+  schedule="@weekly"
+    , start_date=datetime.now()
+    , catchup=False
+    , tags=['api']
+    , description="This dag fetches asset's data available at Brapi"
+)
+def fetch_assets():
+    """
+    Fetches asset's data
+    """
+    http_sensor_task = HttpSensor(
+        task_id = "check_api"
+        , http_conn_id="brapi"
+        , endpoint="quote/BBAS3"
+        , poke_interval=5
+        , timeout=20
+        , mode="poke"
+    )
+
+    db_sensor_task = SQLExecuteQueryOperator(
+        task_id="test_postgres_connection"
+        , conn_id="local_pg"
+        , sql="SELECT 1;"
+        )
+    
+    @task
+    def fetch_data_from_api():
+        """
+        Fetch asset list
+        """
+        endpoint = f"quote/list/"
+        http_hook = HttpHook(method='GET', http_conn_id='brapi')
+        response = http_hook.run(endpoint=endpoint).json().get('stocks')
+        return pd.DataFrame(response)   
+    
+    
+    @task
+    def insert(data):
+        """_summary_
+
+        Args:
+            data (_type_): _description_
+        """
+        table = "assets_raw"
+        hook = PostgresHook(postgres_conn_id='local_pg')
+        engine = hook.get_sqlalchemy_engine()
+        df = pd.DataFrame(data)
+        try: 
+            df.to_sql(table, con=engine, if_exists='replace', index=True)
+            print(f"{table} created succesfully!")
+        except Exception as e:
+            print(e)
+        
+    
+         
+    
+    assets = fetch_data_from_api()   
+    table = insert(assets)
+    http_sensor_task >> db_sensor_task >> assets >> table
+
+fetch_assets()
+    
--- dags/historical_quote.py
+++ dags/historical_quote.py
@@ -52,7 +52,7 @@ def fetch_multiple_tickers():
         that have transactions
         """
         hook = PostgresHook(postgres_conn_id='local_pg')
-        sql = 'SELECT DISTINCT ticker from inv.public.transac'
+        sql = """select distinct stock from assets_raw where type = 'stock'"""
         results = hook.get_records(sql)
         tickers = [row[0] for row in results]
         return tickers
@@ -67,11 +67,16 @@ def fetch_multiple_tickers():
         params = {
             "range": "3mo",
             "interval": "1d",
-        }
-        response = http_hook.run(endpoint=endpoint, data=params).json()
-        historical_records = response.get('results')[0].get('historicalDataPrice')
-        historical_records = [{**d, 'ticker': ticker} for d in historical_records]
-        return historical_records
+        }        
+        try:
+            response = http_hook.run(endpoint=endpoint, data=params).json()
+            historical_records = response.get('results')[0].get('historicalDataPrice')
+            historical_records = [{**d, 'ticker': ticker} for d in historical_records]
+            return historical_records   
+        except Exception as e:
+            print(e)
+
+                     
     
     @task
     def aggregate_results(results):
